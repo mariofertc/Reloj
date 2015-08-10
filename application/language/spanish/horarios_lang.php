e697fb1c4132fc1bb38cbca86c72a239a9d22e0c
Mario Torres <mariofertc@mixmail.com>
2015-08-10 02:45:48 -0500

    Insert of Horario, Tomando forma el horario

---
+++ application/language/spanish/horarios_lang.php
@@ -0,0 +1,10 @@
+<?php
+$lang['horarios_horarios'] = 'Horarios';
+$lang['horarios_successful_add'] = 'Ingreso Satisfactorio del Horario/a ';
+$lang['horarios_successful_update'] = 'Horario actualizado ';
+$lang['horarios_error_add_update'] = 'Fallo el ingreso del Horario';
+$lang['horarios_confirm_delete']='¿Estás seguro(a) que quieres borrar al Horario/s seleccionados?';
+$lang['horarios_none_selected']='No has seleccionado horario para borrar';
+$lang['horarios_successful_deleted']='Has borrado satisfactoriamente a';
+$lang['horarios_one_or_multiple']='horario(s)';
+$lang['horarios_cannot_be_deleted']='No se pudieron borrar los horarios. Uno o más horarios bloqueados.';
--- application/models/Horario_model.php
+++ application/models/Horario_model.php
@@ -1,40 +1,49 @@
 <?php
+
 class Horario_model extends CI_Model {
 
-	public function exist($codigo){
-		$this->db->where(array('codigo'=>$codigo));
-		$result = $this->db->get('horario');
-		//var_dump();
-		if($result->num_rows()>0)
-			return true;
-		else
-			return false;
-	}
-	public function save($data4,$codigo=-1){
-		$response = null;
-	    if(!$this->exist($codigo))
-			$response = $this->db->insert('horario',$data4);
-		else
-			$response = $this->db->update('horario',$data4,array('codigo'=>$codigo));
-		//echo $this->db->last_query();
-		return $response;
-	}
-	public function get_all($num = 0, $offset = 0, $where = null, $order = null){
-		$this->db->where_not_in('deleted', 1);
-		if(!empty($where))
-			$this->db->where($where);
-		$this->db->limit($num, $offset);
-		$this->db->order_by($order);
-		$result = $this->db->get('horario');
-		return $result->result_array();
-	}
-	public function get_info($codigo){
-		$this->db->where(array('codigo'=>$codigo));
-		$result = $this->db->get('horario');
-		return $result->result();
-	}
-        function get_total(){
-		//$this->mongo_db->where(array('deleted' => array('$exists' => false)));
-   		return $this->db->count_all('horario');
+    public function exist($id) {
+        $this->db->where(array('id' => $id));
+        $result = $this->db->get('horario');
+        if ($result->num_rows() > 0)
+            return true;
+        else
+            return false;
+    }
+
+    public function save(&$data, $id = -1) {
+        $response = null;
+        $data['fecha_actualizacion'] = date('Y-m-d H:i:s');
+        if (!$this->exist($id)){
+            $data['fecha_creacion'] = date('Y-m-d H:i:s');
+            $response = $this->db->insert('horario', $data);
+            return $data['id'] = $this->db->insert_id();
         }
+        else
+            $response = $this->db->update('horario', $data, array('id' => $id));
+        //echo $this->db->last_query();
+        return $response;
+    }
+
+    public function get_all($num = 0, $offset = 0, $where = null, $order = null) {
+        $this->db->where_not_in('deleted', 1);
+        if (!empty($where))
+            $this->db->where($where);
+        $this->db->limit($num, $offset);
+        $this->db->order_by($order);
+        $result = $this->db->get('horario');
+        return $result->result_array();
+    }
+
+    public function get_info($id) {
+        $this->db->where(array('id' => $id));
+        $result = $this->db->get('horario');
+        return $result->result();
+    }
+
+    function get_total() {
+        //$this->mongo_db->where(array('deleted' => array('$exists' => false)));
+        return $this->db->count_all('horario');
+    }
+
 }
