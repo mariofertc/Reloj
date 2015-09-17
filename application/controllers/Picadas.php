6ca5d87740e28d529f399e3882cbe820cf5ee643
Mario Torres <mariofertc@mixmail.com>
2015-09-16 23:25:34 -0500

    Completar días no picados
    Se completo con 00:00 los días que no ha picado.

--- application/controllers/Picadas.php
+++ application/controllers/Picadas.php
@@ -201,9 +201,11 @@ class Picadas extends CI_Controller {
         $codigo_reloj = $empleado->id_reloj;
         if ($codigo_reloj) {
             $horario = $this->Horario_model->get_all(100, 0, array('id' => $empleado->id_horario));
-            $picadas = $this->Picada_model->get_all(1000, 0, array('codigo' => $codigo_reloj, 'fecha_picada >=' => date('Y-m-d', strtotime($fecha_desde))
-                , 'fecha_picada<=' => date('Y-m-d', strtotime($fecha_hasta))), 'fecha_picada ASC');
-            $resp= asignar_picadas($horario[0], $picadas);
+            $desde = date('Y-m-d', strtotime($fecha_desde));
+            $hasta = date('Y-m-d', strtotime($fecha_hasta));
+            $picadas = $this->Picada_model->get_all(1000, 0, array('codigo' => $codigo_reloj, 'fecha_picada >=' => $desde
+                , 'fecha_picada<=' => $hasta), 'fecha_picada ASC');
+            $resp= asignar_picadas($horario[0], $picadas, new DateTime($desde), new DateTime($hasta));
             echo json_encode(array('response' => true, "message" => "Empleado sin código de reloj asignado", "picadas"=>$resp,"horario"=>$horario[0], "empleado"=>$empleado));
         } else {
             echo json_encode(array('response' => false, "message" => "Empleado sin código de reloj asignado"));
--- application/helpers/picadas_helper.php
+++ application/helpers/picadas_helper.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 if (!function_exists('asignar_picadas')) {
 
-    function asignar_picadas($horario, $picadas) {
+    function asignar_picadas($horario, $picadas, $desde, $hasta) {
         //extract($options);
         $rango = array();
         $max_minutos_extras = 60 * 4;
@@ -24,15 +24,30 @@ if (!function_exists('asignar_picadas')) {
         //Chequea si hubo ya una picada anterior.
         $cll_picadas = array();
         $cll_observacion = array();
-        $idx_picadas = -1;
-        $idx_picada = 0;
+        $idx_picadas = 0;
+        $idx_picada = -1;
         $dia_anterior = new DateTime();
+        $dia_falta = null;
         foreach ($picadas as $registro) {
             $tiempo_picada = new DateTime($registro->fecha_picada);
             //Ver si es la fecha del ingreso del trabajo, y completa con los días de faltas.
-            //if($tiempo_picada >)
+            //Cambio de Día.
             if ($tiempo_picada->format('d') != $dia_anterior->format('d')) {
-                $idx_picadas++;
+                
+                //Chequea y rellena los días faltados.
+                $dia_falta = is_null($dia_falta) ? dia_horario($desde,$horario['dias']) : $dia_falta;
+                while ($dia_falta->format('d/m/y') < $tiempo_picada->format('d/m/y')) {
+                    $idx_picadas ++;
+                    for( $idx_falta = 0; $idx_falta < count($rango); $idx_falta ++) {
+                        $cll_picadas[$idx_picadas][] = get_comodin($dia_falta, $rango, $idx_falta);
+                    }
+                    if($dia_falta != $tiempo_picada)
+                        $dia_falta = siguiente_dia_horario($dia_falta, $horario['dias']);
+                }
+                $dia_falta = siguiente_dia_horario($dia_falta, $horario['dias']);
+
+                //Indice general de días.
+                $idx_picadas = count($cll_picadas);
                 if (isset($cll_picadas[$idx_picadas != 0 ? $idx_picadas - 1 : $idx_picadas])) {
                     while (count($rango) > count($cll_picadas[$idx_picadas != 0 ? $idx_picadas - 1 : $idx_picadas])) {
                         $cll_picadas[$idx_picadas != 0 ? $idx_picadas - 1 : $idx_picadas][] = get_comodin($dia_anterior, $rango, count($cll_picadas[$idx_picadas != 0 ? $idx_picadas - 1 : $idx_picadas]));
@@ -59,18 +74,32 @@ if (!function_exists('asignar_picadas')) {
                     $idx_picada ++;
                 }
             }
+            if($picadas[count($picadas)-1] == $registro){
+                //Chequea y rellena los días faltados.
+                $dia_falta = siguiente_dia_horario($tiempo_picada, $horario['dias']);
+                while ($hasta >= new DateTime($dia_falta->format('m/d/Y'))) {
+                
+                    $idx_picadas ++;
+                    for( $idx_falta = 0; $idx_falta < count($rango); $idx_falta ++) {
+                        $cll_picadas[$idx_picadas][] = get_comodin($dia_falta, $rango, $idx_falta);
+                    }
+                    $dia_falta = siguiente_dia_horario($dia_falta, $horario['dias']);
+                }
+            }
         }
-            $tot_minutos_trabajados = 0;
-            $tot_minutos_extras = 0;
-            $tot_minutos_atrasos = 0;
+
+        //Observaciones
+        $tot_minutos_trabajados = 0;
+        $tot_minutos_extras = 0;
+        $tot_minutos_atrasos = 0;
         for ($idx_arreglo = 0; $idx_arreglo < count($cll_picadas); $idx_arreglo++) {
-            //$cll_response[$idx_arreglo]['cll_picadas'] = $cll_picadas[$idx_arreglo];
             $obs = new stdClass();
             $minutos_trabajados = 0;
             $minutos_atrasos = 0;
             $idx_anterior = 0;
             $picada_comodin = null;
             $picada_anterior = null;
+            //var_dump($cll_picadas);
             for ($idx = 0; $idx < count($cll_picadas[$idx_arreglo]); $idx++) {
                 $picada = $cll_picadas[$idx_arreglo][$idx];
                 if ($idx % 2 == 1) {
@@ -80,13 +109,9 @@ if (!function_exists('asignar_picadas')) {
                             $picada_anterior = $picada_comodin;
                     } else
                         $picada_anterior = $picada_comodin;
-
-                    //$picada_anterior = $cll_picadas[$idx_arreglo][$idx_anterior];
                     if (!$picada->fallo) {
                         $minutos_trabajados += $picada->minutos - $picada_anterior->minutos;
                     }
-                    //$obs->minutos_trabajados += $picada->picada_red + $picada_anterior->picada_red;
-                    //$res = $picada->minutos - $picada_anterior->minutos;
                 } else if ($picada->diferencia_minutos < 0) {
                     $minutos_atrasos = $minutos_atrasos - $picada->diferencia_minutos;
                 }
@@ -117,55 +142,41 @@ if (!function_exists('asignar_picadas')) {
 
         $resumen->tot_horas_atrasos = adherir_zero(intval($tot_minutos_atrasos / 60, 0));
         $resumen->tot_minutos_atrasos = adherir_zero(abs($tot_minutos_atrasos % 60));
-//        return $cll_picadas;
-        return array('cll_picadas' => $cll_picadas, 'cll_observacion' => $cll_observacion,'resumen'=>$resumen);
-        //var_dump($cll_picadas);
-        //Primer caso, todo bien.
-        /* if (cll_picadas.length == rango.length) {
-          append = "<div class='form-group form-inline'>";
-          append += "<label>" + tiempo[0] +"</label>";
-          total_minutos = 0;
-          $.each(cll_picadas, function(idx, pica) {
-          hora_picada = parseInt(pica.split(":")[0]);
-          minuto_picada = parseInt(pica.split(":")[1]);
-          if(idx%2 == 1){
-          total_minutos += (hora_picada * 60 + minuto_picada) - minutos_picada;
-          }
-          minutos_picada = hora_picada * 60 + minuto_picada;
-          diff = minutos_picada - rango[idx];
+        return array('cll_picadas' => $cll_picadas, 'cll_observacion' => $cll_observacion, 'resumen' => $resumen);
+    }
 
-          if(idx%2 == 1){
-          if(diff > 0){
-          //Atrasos
-          append += '<span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span><span id="inputWarning2Status" class="sr-only">(warning)</span>';
-          }
-          if(diff < 0){
-          //Horas Extras
-          append +=  '<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span><span id="inputSuccess2Status" class="sr-only">(success)</span>';
-          }
-          }else{
-          diff = diff * -1;
-          if(diff > 0){
-          //Atrasos
-          append += '<span class="glyphicon glyphicon-warning-sign form-control-feedback" aria-hidden="true"></span><span id="inputWarning2Status" class="sr-only">(warning)</span>';
-          }
-          if(diff < 0){
-          //Horas Extras
-          append +=  '<span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span><span id="inputSuccess2Status" class="sr-only">(success)</span>';
-          }
-          }
-          append += "<input value='" + pica + " " + diff + "' class='form-control' readonly=true/>";
-          });
-          append += "<label>Horas Trabajadas:" + Math.floor(total_minutos/60) + ":" + (total_minutos%60)+"</label>";
-          horas_obs = (total_minutos-480)/60
-          if(horas_obs > 0)
-          append += "<label> - HE:" + Math.floor((total_minutos-480)/60) + ":" + ((total_minutos-480)%60)+"</label>";
-          else
-          append += "<label> - HA:" + Math.floor((total_minutos-480)/60) + ":" + ((total_minutos-480)%60)+"</label>";
-          // append += "<label>Horas Trabajadas:" + (total_minutos/60).toFixed(2) + "</label>";
-          append += "</div>";
-          $("#result").append(append);
-          cll_picadas = []; */
+}
+
+if (!function_exists('siguiente_dia_horario')) {
+
+    function siguiente_dia_horario($dia_falta, $dias) {
+        $dias_horario = json_decode($dias, TRUE);
+        $i = 7;
+        do {
+            $dia = $dia_falta->modify('+1 day');
+            foreach($dias_horario as $d){
+                if (strcasecmp(substr(dia_semana($dia),-4), substr($d,-4))== 0){
+                    return $dia;
+                }
+            }
+        }while ($i-- > 0);
+    }
+
+}
+if (!function_exists('dia_horario')) {
+
+    function dia_horario($dia_falta, $dias) {
+        $dias_horario = json_decode($dias, TRUE);
+        $i = 7;
+        do {
+            
+            foreach($dias_horario as $d){
+                if (strcasecmp(substr(dia_semana($dia_falta),-4), substr($d,-4))== 0){
+                    return $dia_falta;
+                }
+            }
+            $dia_falta->modify('+1 day');
+        }while ($i-- > 0);
     }
 
 }
@@ -187,6 +198,7 @@ if (!function_exists('adherir_comodin')) {
         $obj->dia = $tiempo_picada->format("d/m/Y");
         $obj->tiempo = "00:00";
         $obj->posicion = $idx_picada;
+        $obj->dia_texto = dia_semana($tiempo_picada);
         $obj->picada_red_horario = $rango[$idx_picada];
         $obj->picada_red = "s/r";
         $obj->minutos = 0;
@@ -203,16 +215,18 @@ if (!function_exists('acomoda_ubicacion')) {
         $picada_reducida = $picada_minutos - $rango[0];
         $result = array();
         for ($i = 0; $i < count($rango); $i++) {
-            //echo $i;
             $picada_reducida_horario = $rango[$i] - $rango[0];
             $result[$i] = array(abs($picada_reducida - $picada_reducida_horario), $i);
         }
         sort($result);
+
+        //Para obtener el día en español.
         $best_idx = $result[0][1];
         $obj = new stdClass;
         $obj->picada = $tiempo_picada;
         $obj->dia = $tiempo_picada->format("d/m/Y");
         $obj->tiempo = $tiempo_picada->format("H:i");
+        $obj->dia_texto = dia_semana($tiempo_picada);
         $obj->fallo = null;
         $obj->posicion = $best_idx;
         $obj->picada_red_horario = $picada_reducida_horario;
@@ -221,6 +235,16 @@ if (!function_exists('acomoda_ubicacion')) {
         $obj->diferencia_minutos = $rango[$best_idx] - $picada_minutos;
         //$cll_picadas[] = $obj;
         return $obj;
+    }
+
+}
+
+
+if (!function_exists('dia_semana')) {
+
+    function dia_semana($tiempo_picada) {
+        $dias = array("Domingo", "Lunes", "Martes", "Mi&eacute;rcoles", "Jueves", "Viernes", "S&aacute;bado");
+        return $dias[$tiempo_picada->format("w")];
     }
 
 }
